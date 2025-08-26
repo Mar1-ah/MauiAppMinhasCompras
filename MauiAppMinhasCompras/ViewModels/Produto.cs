3eb17193ca2a4b35e6965b23aa1a47442d027643
Mar1-ah <mariah.milanesi@etec.sp.gov.br>
2025-08-26 19:37:34 -0300

    alterações da agenda 4 de ds

---
+++ MauiAppMinhasCompras/ViewModels/Produto.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MauiAppMinhasCompras.ViewModels
+{
+    internal class ProductsViewModel
+    {
+    }
+}
+
+public class Product
+{
+    public required string Name { get; set; }
+    public string Description { get; set; }
+}
